ffc9980b2cc8dfd3b01673e4f94d460aa2894eed
Thiago Prado de Mello Branco <thiagopradodemellobranco@MacBook-Pro-de-Thiago.local>
2023-04-10 09:45:53 -0300

    foi alterado tabela medicos incluindo campo telefone

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
@@ -17,7 +18,7 @@ public class MedicoController {
     private MedicoRepository repository;
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 }
